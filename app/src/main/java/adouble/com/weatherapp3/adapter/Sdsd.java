f038d8eefbf6c2d4aea5ccaef68820a977bdffe0
Rakhimjon <rahimjon@double.uz>
2016-12-01 11:45:30 +0500

    Testdsd

--- app/src/main/java/adouble/com/weatherapp3/adapter/Sdsd.java
+++ app/src/main/java/adouble/com/weatherapp3/adapter/Sdsd.java
@@ -1,8 +1,11 @@
 package adouble.com.weatherapp3.adapter;
 
+import java.util.StringTokenizer;
+
 /**
  * Created by root on 12/1/16.
  */
 
 public class Sdsd {
+    public StringTokenizer jhkh;
 }
